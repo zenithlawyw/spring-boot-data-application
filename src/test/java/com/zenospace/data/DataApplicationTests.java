26ad151496ebc22311202eb14afc717c9bd657ac
Zenith Law <zenith@zenithlaw.com>
2022-10-24 22:19:21 +0100

    Initial commit

---
+++ src/test/java/com/zenospace/data/DataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenospace.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
